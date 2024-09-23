56103f189d93b420192813bbff495952fce77c86
Kieran Huggins <kieran@kieran.ca>
2024-09-22 22:42:28 -0700

    fix git dir, install deps only if missing

--- .config/install.sh
+++ .config/install.sh
@@ -2,26 +2,28 @@
 
 # check out dotfiles
 cd $HOME
-git clone https://github.com/kieran/.dotfiles.git .dotfiles.git
+git clone --bare https://github.com/kieran/.dotfiles.git .dotfiles.git
 
 # set git alias
 git config --global alias.dotfiles '!git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'
 
 # checkout files
-git dotfiles checkout main
+mv .zshrc .zshrc.bak 2>/dev/null
+mv .config/starship.toml .config/starship.toml.bak 2>/dev/null
+git dotfiles checkout --force main
 
 # install nerd font via getnf
-curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
+type getnf || curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
 getnf -i FiraCode
 
 # install zap
-zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+type zap || zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 
 # install starship
-curl -sS https://starship.rs/install.sh | sh
+type starship || curl -sS https://starship.rs/install.sh | sh
 
 # install mcfly
-brew install mcfly
+type mcfly || brew install mcfly
 
 # reload terminal
 source ~/.zshrc
